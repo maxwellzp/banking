b865a4b6296a23a9caa2c53e53c2f1d051b29fce
Evtyagin Maksim <evtyagin.maksim@gmail.com>
2023-06-04 16:06:12 +0300

    Development

--- app/app.go
+++ app/app.go
@@ -3,13 +3,29 @@ package app
 import (
 	"banking/domain"
 	"banking/service"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
+func sanityCheck() {
+	if os.Getenv("SERVER_ADDRESS") == "" ||
+		os.Getenv("SERVER_PORT") == "" ||
+		os.Getenv("DB_USER") == "" ||
+		os.Getenv("DB_PASSWD") == "" ||
+		os.Getenv("DB_ADDR") == "" ||
+		os.Getenv("DB_PORT") == "" ||
+		os.Getenv("DB_NAME") == "" {
+		log.Fatal("Environment variable not defined....")
+	}
+}
+
 func Start() {
+
+	sanityCheck()
 
 	// mux := http.NewServeMux()
 	router := mux.NewRouter()
@@ -23,10 +39,8 @@ func Start() {
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
-	// router.HandleFunc("/greet", greet).Methods(http.MethodGet)
-	// router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
-	// router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
-
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
